04b2b3f0905fe7e2cb011ba8133800ff805ba6e0
Evgeny Li <exaspark@gmail.com>
2013-06-28 00:48:54 +0400

    make changeable file's visibility, fix indentions (4 spaces)

--- file_extensions.js
+++ file_extensions.js
@@ -1,41 +1,66 @@
 var allFiles          = getAllElementsWithAttribute('class', 'meta');
-var allFileNames      = getAllFileNames(allFiles);
-var allFileExtensions = getAllFileExtensions(allFileNames);
+var allFileExtensions = getAllFileExtensions(allFiles);
 
 // send allFileExtensions to popup.js
-chrome.extension.sendRequest(allFileExtensions);
+chrome.extension.sendMessage(allFileExtensions);
+
+// ----------------------------------------------------------------------------
+
+chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+    if(message.action === 'checkboxToggled') {
+        for (var i = 0; i < allFiles.length; i++) {
+            var fileExtension = getFileExtension(allFiles[i]);
+            if (fileExtension === message.fileExtension) {
+                toggleFileVisibility(allFiles[i], message.visible);
+            }
+        }
+    }
+});
+
+function toggleFileVisibility(file, visible) {
+    var display;
+    if (visible) {
+        display = 'block';
+    }
+    else {
+        display = 'none';
+    }
+
+    var parentNode = file.parentNode;
+    parentNode.style.display = display;
+}
 
 // ----------------------------------------------------------------------------
 
 function getAllElementsWithAttribute(attribute, value) {
-  var matchingElements = [];
-  var allElements = document.getElementsByTagName('*');
-  for (var i = 0; i < allElements.length; i++) {
-    if (allElements[i].getAttribute(attribute) == value) {
-      matchingElements.push(allElements[i]);
+    var matchingElements = [];
+    var allElements = document.getElementsByTagName('*');
+    for (var i = 0; i < allElements.length; i++) {
+        if (allElements[i].getAttribute(attribute) == value) {
+        matchingElements.push(allElements[i]);
+        }
     }
-  }
-  return matchingElements;
+    return matchingElements;
 }
 
-function getAllFileNames(allFiles) {
-  for (var i = 0; i < allFiles.length; i++) {
-    allFiles[i] = allFiles[i].getAttribute('data-path');
-  }
-  return allFiles;
-}
-
-function getAllFileExtensions(allFileNames) {
-  var allFileExtensions = [];
-  for (var i = 0; i < allFileNames.length; i++) {
-    var fileExtension = getFileExtension(allFileNames[i]);
-    if (allFileExtensions.indexOf(fileExtension) == -1) {
-      allFileExtensions.push(fileExtension);
+function getAllFileExtensions(allFiles) {
+    var allFileExtensions = [];
+    for (var i = 0; i < allFiles.length; i++) {
+        var fileExtension = getFileExtension(allFiles[i]);
+        if (allFileExtensions.indexOf(fileExtension) === -1) {
+            allFileExtensions.push(fileExtension);
+        }
     }
-  }
-  return allFileExtensions;
+    return allFileExtensions;
 }
 
-function getFileExtension(fileName) {
-  return fileName.split('.').pop();
+function getFileExtension(file) {
+    var fileName = file.getAttribute('data-path');
+
+    if (fileName.indexOf('.') !== -1) {
+        return '.' + fileName.split('.').pop();
+    }
+    else {
+        return fileName;
+    }
 }
--- popup.js
+++ popup.js
@@ -1,40 +1,46 @@
 // TODO
-// - hide files by extension
 // - disable extension if not github
 // - make it more pretty
+// - add documentation
 // - add settings (default active file extensions)
 
 // execute file_extensions.js in the current tab
 window.onload = function() {
-  chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.executeScript(tab.id, { file: 'file_extensions.js', allFrames: true });
-  });
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.executeScript(tab.id, { file: 'file_extensions.js' });
+    });
 };
 
 // get allFileExtensions from file_extensions.js
-chrome.extension.onRequest.addListener(function(allFileExtensions) {
-  var divFileExtensions = document.getElementById('fileExtensions');
+chrome.extension.onMessage.addListener(function(allFileExtensions) {
+    var divFileExtensions = document.getElementById('fileExtensions');
 
-  for (var i = 0; i < allFileExtensions.length; i++) {
-    var checkbox = createFileExtensionCheckbox(allFileExtensions[i]);
-    divFileExtensions.appendChild(checkbox);
-  }
+    for (var i = 0; i < allFileExtensions.length; i++) {
+        var divCheckbox = createFileExtensionCheckbox(allFileExtensions[i]);
+        divFileExtensions.appendChild(divCheckbox);
+    }
 });
 
 function createFileExtensionCheckbox(name) {
-  var div      = document.createElement('div');
-  var nameDiv  = document.createElement('div');
-  var checkbox = document.createElement('input');
+    var div      = document.createElement('div');
+    var nameDiv  = document.createElement('div');
+    var checkbox = document.createElement('input');
 
-  nameDiv.innerHTML = name;
-  nameDiv.className = 'name';
+    nameDiv.innerHTML = name;
+    nameDiv.className = 'name';
 
-  checkbox.type    = 'checkbox';
-  checkbox.checked = true;
-  checkbox.id      = name;
+    checkbox.type    = 'checkbox';
+    checkbox.checked = true;
+    checkbox.id      = name;
 
-  div.appendChild(nameDiv);
-  div.appendChild(checkbox);
+    div.appendChild(nameDiv);
+    div.appendChild(checkbox);
 
-  return div;
+    checkbox.onchange = function() {
+        chrome.tabs.getSelected(null, function(tab) {
+            chrome.tabs.sendMessage(tab.id, { action: 'checkboxToggled', fileExtension: checkbox.id, visible: checkbox.checked }, function(response) {});
+        });
+    };
+
+    return div;
 }
